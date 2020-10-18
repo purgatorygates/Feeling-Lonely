72faa8355c34188bc34c6759902b1b58ddded2ed
purgatorygates <ulisesrivero1984@gmail.com>
2020-10-18 16:17:02 -0400

    finished styling and code

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const chatbox = document.querySelector("#chatbox");
-const lonelyButton = document.querySelector("button");
-const me = ["Me", "Myself", "I"];
+const button = document.querySelector("button");
+const user = ["Me", "Myself", "I"];
 let id = 2;
 let apiURL = "https://api.chucknorris.io/jokes/random";
 form.addEventListener("submit", handleForm);
-lonelyButton.addEventListener("click", handleChuck);
+button.addEventListener("click", chuckJokes);
 function handleForm(e) {
   e.preventDefault();
   id++;
-  const randomNumber = Math.floor(Math.random() * me.length);
-  const newMessage = document.createElement("div");
+  const randomNumber = Math.floor(Math.random() * user.length);
+  const newDm = document.createElement("div");
   const timeStamp = new Date().toLocaleTimeString("en-US");
-  newMessage.className = "message";
-  newMessage.setAttribute("id", id);
-  newMessage.innerHTML = `
+  newDm.className = "message";
+  newDm.setAttribute("id", id);
+  newDm.innerHTML = `
     <span>${timeStamp}</span>
-    <span class="sender">${me[randomNumber]}</span>
+    <span class="sender">${user[randomNumber]}</span>
     <span>${input.value}</span>
     <span class="delete" onclick="deleteMessage(${id})">❌</span>
   `;
-  chatbox.appendChild(newMessage);
+  chatbox.appendChild(newDm);
   input.value = "";
 }
 function deleteMessage(id) {
   const message = document.getElementById(id);
   message.remove();
 }
-function handleChuck() {
+function chuckJokes() {
   id++;
   fetch(apiURL)
     .then((response) => response.json())
